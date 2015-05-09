0df61548decc9ac1b194a3e0ba2de9b07bcc6487
Martijn de Vos <mdmartijndevos@gmail.com>
2015-05-09 17:27:24 +0200

    Initial Commit

---
+++ MyTwitchChannel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyTwitchChannel
+//
+//  Created by Martijn de Vos on 09-05-15.
+//  Copyright (c) 2015 martijndevos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
